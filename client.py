1790a9e0eb63644349369900b3184c46cfebb892
ThiagoMachadoUFG <88293701+ThiagoMachadoUFG@users.noreply.github.com>
2023-06-28 21:10:39 -0300

    Update client.py

--- client.py
+++ client.py
@@ -5,5 +5,12 @@ class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
   print (conn.root.exposed_value())
   conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
+  conn.root.exposed_append(6)
+  conn.root.exposed_append(12)
+  conn.root.exposed_append(8)
+  conn.root.exposed_append(2)
   print (conn.root.exposed_value())   # Print the result
+  print (conn.root.exposed_remove(2))
+  print (conn.root.exposed_search(2))
+  conn.root.exposed_sort()
+  print (conn.root.exposed_value()) 

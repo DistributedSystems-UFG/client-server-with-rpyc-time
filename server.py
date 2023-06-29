1ce303f061c92dec17c25586df5d621492b80253
ThiagoMachadoUFG <88293701+ThiagoMachadoUFG@users.noreply.github.com>
2023-06-28 21:04:52 -0300

    Update server.py
    added funtions

--- server.py
+++ server.py
@@ -12,6 +12,24 @@ class DBList(rpyc.Service):
   def exposed_value(self):
     return self.value
 
+def exposed_search(self, data):
+    return [item for item in self.value if item == data]
+
+def exposed_sort(self, data):
+  DBLength = len(DBList)
+  for i in range(DBLength):
+      swapped = False
+      for j in range(0, DBLength-i-1):
+          if DBList[j] > DBList[j+1]:
+              DBList[j], DBList[j+1] = DBList[j+1], DBList[j]
+              swapped = True
+      if (swapped == False):
+          break
+
+def exposed_remove(self, data):
+    self.value = [item for item in self.value if item != data]
+    return self.value
+
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
   server.start()
